ba16a04abeb5bed73881ebfb38e98e0ca8e515f6
marujore <rennanmarujo@gmail.com>
2021-07-01 17:22:36 -0300

    update examples

--- examples/example_harm_l7.py
+++ examples/example_harm_l7.py
@@ -20,7 +20,7 @@ sr_dir = '/path/to/L7/SR/images/'
 target_dir = '/path/to/output/NBAR/'
 scene_id = 'THE_SCENE_ID'
 
-landsat_harmonize('LE7', scene_id, sr_dir, target_dir)
+landsat_harmonize(scene_id, sr_dir, target_dir)
 
 end = time.time()
 print(f'Duration time: {end - start}')
